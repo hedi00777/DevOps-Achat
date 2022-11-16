2e191d6c89fa2a810332d939f9004506cc8c5ba8
hedi00777 <mohamedhedi.benabdelgheni@esprit.tn>
2022-11-16 23:07:16 +0100

    change commit

--- src/main/java/tn/esprit/rh/achat/controllers/SecteurActiviteController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/SecteurActiviteController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RequestMapping("/secteurActivite")
 @CrossOrigin("*")
 public class SecteurActiviteController {
-
+//oop
 	@Autowired
 	ISecteurActiviteService secteurActiviteService;
 	
